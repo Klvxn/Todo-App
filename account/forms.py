a7dd486f7cc733be9f06cd09defefbe07e9c3731
Kelvxn <akpuluchibuike@gmail.com>
2022-06-20 23:10:34 +0100

    Redesigned & refactorred code

--- account/forms.py
+++ account/forms.py
@@ -2,8 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
+# Forms
 class RegisterUser(UserCreationForm):
     email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ["username", "email", "password1", "password2"]
--- mytodo/urls.py
+++ mytodo/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 
 from . import views
 
-app_name = 'mytodo'
+
+app_name = "mytodo"
 
 urlpatterns = [
-    path('', views.indexpage, name='indexpage'),
-    path('home/', views.homepage, name='homepage'),
-    path('todos/<int:pk>/', views.detailpage, name='detailpage'),
-    path('todos/create-todo/', views.create_todo, name='create-todo'),
-    path('todos/<int:pk>/edit-todo/', views.edit_todo, name='edit-todo'),
-    path('todos/<int:pk>/delete-todo/', views.delete_todo, name='delete-todo'),
-    path('todos/completed-todos/', views.completed_todos, name='completed-todos'),
-    path('todos/uncompleted-todos/', views.uncompleted_todos, name='uncompleted-todos'),
-
+    path("", views.indexpage, name="indexpage"),
+    path("home/", views.homepage, name="homepage"),
+    path("todos/<int:pk>/", views.detailpage, name="detailpage"),
+    path("todos/create-todo/", views.create_todo, name="create-todo"),
+    path("todos/<int:pk>/edit-todo/", views.edit_todo, name="edit-todo"),
+    path("todos/<int:pk>/delete-todo/", views.delete_todo, name="delete-todo"),
+    path("todos/completed-todos/", views.completed_todos, name="completed-todos"),
+    path("todos/incompleted-todos/", views.incompleted_todos, name="incompleted-todos"),
 ]
