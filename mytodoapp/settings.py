691066501e1821e27394833844d9dc90bf6bfeb8
Chibuike Akpulu <akpulukelvin@gmail.com>
2022-10-08 08:14:39 +0100

    Update settings.py

--- mytodoapp/settings.py
+++ mytodoapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "safe-space"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".railway.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
